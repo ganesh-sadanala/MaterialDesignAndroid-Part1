d2b6c11baf4257112fbca2a721b38330247458f1
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2019-06-22 11:54:21 +0530

    Add widgets to StandaloneToolbar

--- MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/StandaloneToolbar.java
+++ MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/StandaloneToolbar.java
@@ -1,14 +1,20 @@
 package com.ganesh.materialdesignpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class StandaloneToolbar extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
+        toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitle("StandAloneToolbar!");
+        toolbar.setSubtitle("By Ganesh");
+        toolbar.setNavigationIcon(R.drawable.ic_navigation_back);
     }
 }
