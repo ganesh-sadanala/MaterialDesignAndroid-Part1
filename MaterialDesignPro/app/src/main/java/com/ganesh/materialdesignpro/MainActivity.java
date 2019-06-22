34c5c91023852ac1082660ac5b5efe7cdbef6ea2
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2019-06-22 11:34:59 +0530

    Add StandaloneToolbar

--- MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/MainActivity.java
+++ MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/MainActivity.java
@@ -2,8 +2,10 @@ package com.ganesh.materialdesignpro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
-        {
+//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
+//        {
+//
+//        }
+//        else
+//        {
+//
+//        }
+    }
 
-        }
-        else
-        {
+    public void ShowStandAloneToolbar(View view) {
+        Intent intent=new Intent(this,StandaloneToolbar.class);
+        startActivity(intent);
+    }
 
-        }
+    public void ShowToolbarAsActionBar(View view) {
+    }
+
+    public void ShowContextualMenu(View view) {
     }
 }
---
+++ MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/StandaloneToolbar.java
@@ -0,0 +1,14 @@
+package com.ganesh.materialdesignpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StandaloneToolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+    }
+}
