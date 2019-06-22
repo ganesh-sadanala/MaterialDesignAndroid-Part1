0fcd79481d09994c6892ba4bc01f7fef0133b45f
ganesh-sadanala <40536512+ganesh-sadanala@users.noreply.github.com>
2019-06-22 10:23:32 +0530

    Compatibility through java code

--- MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/MainActivity.java
+++ MaterialDesignPro/app/src/main/java/com/ganesh/materialdesignpro/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ganesh.materialdesignpro;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 }
